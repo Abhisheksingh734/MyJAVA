89cb8335df9696fa97f6100282cc3bc44bd2088b
Abhishek <abhisheksinghasr734@gmail.com>
2022-12-06 23:38:16 +0530

    Greetings in java

---
+++ GreetingMsg.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class GreetingMsg {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        String Chars = in.nextLine();
+        System.out.println("Good morning "+Chars);
+    }
+}
