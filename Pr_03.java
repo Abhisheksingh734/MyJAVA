e9ae3ed357016120b565fd940a5e7bbac24f0ba2
Abhishek <abhisheksinghasr734@gmail.com>
2022-12-06 23:39:41 +0530

    Practice java

---
+++ Pr_03.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Pr_03 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int P=in.nextInt();
+        int T=in.nextInt();
+        int R=in.nextInt();
+
+        int SI =(P*R*T)/100;
+        System.out.println(SI);
+
+
+
+    }
+}
